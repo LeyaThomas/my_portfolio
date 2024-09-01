6ad234ce0ecff6c1c6d1978f4fce8506c6a357a1
LeyaThomas <leyathomas1352002@gmail.com>
2024-09-02 00:51:54 +0530

    contact page updated

--- src/components/contact/index.jsx
+++ src/components/contact/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import PageHeaderContent from "../PageHeader";
-import {BsInfoCircleFill} from 'react-icons/bs'
+import {BsInfoCircleFill} from 'react-icons/bs';
+import './styles.scss';
 
 const Contact = () => {
     return(
@@ -9,6 +10,26 @@ const Contact = () => {
             headerText="Contact"
             icon={<BsInfoCircleFill size={40} />}
             />
+            <div className="contact__content">
+                <h3 className="contact__content__header-text">Let's Talk</h3>
+                <div className="contact__content__form">
+                    <div className="contact__content__form__control">
+                        <div >
+                            <input required name="name" className="inputName" type={'text'}/>
+                            <label htmlFor="name" className="labelName">Name</label>
+                        </div>
+                        <div>
+                        <input required name="email" className="inputEmail" type={'text'}/>
+                        <label htmlFor="email" className="labelEmail">Email</label>
+                        </div>
+                        <div>
+                        <textarea required name="desc" className="inputDesc" type={'text'} rows={5}/>
+                        <label htmlFor="desc" className="labeldesc">Description</label>
+                        </div>
+                    </div>
+                    <button>Submit</button>
+                </div>
+            </div>
         </section>
     )
 }
