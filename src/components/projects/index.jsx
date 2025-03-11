c972fc8fed26d4f6802abc3e2d551249cdd31b48
LeyaThomas <leyathomas1352002@gmail.com>
2025-03-11 10:22:32 +0530

    new project added

--- src/components/projects/index.jsx
+++ src/components/projects/index.jsx
@@ -1,65 +1,65 @@
 import React from "react";
 import { useState } from "react";
-import {BsInfoCircleFill} from 'react-icons/bs'
+import { BsInfoCircleFill } from 'react-icons/bs'
 import PageHeaderContent from "../PageHeader";
 import img1 from "../../images/proj_mgmnt.avif";
-import img2 from "../../images/web_boot.jpg";
+import img2 from "../../images/blog.webp";
 import img3 from "../../images/mozilla.png";
 import img4 from "../../images/ts.png";
 import './styles.scss';
 
-const projectsData =[
+const projectsData = [
     {
-        id:1,
+        id: 1,
         name: "Project Management App",
         image: img1,
-        link : 'https://github.com/LeyaThomas/pma'
+        link: 'https://github.com/LeyaThomas/pma'
     },
-    {
-        id:2,
-        name: "Web Bootcamp projects",
+     {
+        id: 2,
+        name: "Blog Website",
         image: img2,
-        link : 'https://github.com/LeyaThomas/Web_Development_Tasks'
-    },
-    {
-        id:3,
-        name: "Mozilla Splash",
-        image: img3,
-        link : 'https://github.com/LeyaThomas/mozillasplash'
-    },
-    {
-        id:4,
-        name: "TypeScript",
-        image: img4,
-        link : 'https://github.com/LeyaThomas/TS_studyjam_tasks'
-    }
+        link: 'https://github.com/LeyaThomas/guarded_words.git'
+     },
+    // {
+    //     id: 3,
+    //     name: "Mozilla Splash",
+    //     image: img3,
+    //     link: 'https://github.com/LeyaThomas/mozillasplash'
+    // },
+    // {
+    //     id: 4,
+    //     name: "TypeScript",
+    //     image: img4,
+    //     link: 'https://github.com/LeyaThomas/TS_studyjam_tasks'
+    // }
 ]
 
- const filterData = [
-    {
-        filterId : 1,
-        label : "All"
-    },
-    {
-        filterId : 2,
-        label : "Development"
-    },
-    {
-        filterId : 3,
-        label : "Design"
-    }
-]
+// const filterData = [
+//     {
+//         filterId: 1,
+//         label: "All"
+//     },
+//     {
+//         filterId: 2,
+//         label: "Development"
+//     },
+//     {
+//         filterId: 3,
+//         label: "Design"
+//     }
+// ]
 
 const Project = () => {
 
     const [filteredValue, setFilteredValue] = useState(1)
-    const  [hoveredValue, setHoveredValue] = useState(null)
+    const [hoveredValue, setHoveredValue] = useState(null)
 
-    function handleFilter(currentId){
+    function handleFilter(currentId) {
         setFilteredValue(currentId)
     };
 
-    function handleHover(index){
+    function handleHover(index) {
         setHoveredValue(index)
     }
 
@@ -70,18 +70,18 @@ const Project = () => {
     const filteredItems = filteredValue === 1 ? projectsData : projectsData.filter(item => item.id === filteredValue)
 
     console.log(filteredItems);
-       
-    return(
+
+    return (
         <section id="project" className="project">
             <PageHeaderContent
-            headerText="My Projects"
-            icon={<BsInfoCircleFill size={40} />}
+                headerText="My Projects"
+                icon={<BsInfoCircleFill size={40} />}
             />
             <div className="project__content">
-                <ul className="project__content__filter">
+                {/* <ul className="project__content__filter">
                     {
                         filterData.map(item => (
-                            <li className={item.filterId === filteredValue ? 'active' : ''} onClick={()=>handleFilter(item.filterId)} key={item.filterId}>
+                            <li className={item.filterId === filteredValue ? 'active' : ''} onClick={() => handleFilter(item.filterId)} key={item.filterId}>
 
                                 {
                                     item.label
@@ -90,27 +90,27 @@ const Project = () => {
                         ))
                     }
 
-                </ul>
+                </ul> */}
                 <div className="project__content__cards">
                     {
-                        projectsData.map((item,index) => (
-                            <div key={`cardItems${item.name.trim()}`} className="project__content__cards__item" onMouseEnter={() =>handleHover(index)} onMouseLeave={() =>handleHover(null)}>
-                               <div className="project__content__cards__item__img-wrapper">
-                                <a>
-                                    <img src={item.image} alt={item.name} />
-                                </a>
-                               </div>
-                               <div className="overlay">
-                                 {
-                                   index === hoveredValue && (
-                                   <div>
-                                   <p>{item.name}</p>
-                                   <button>Visit</button>
-                                   </div>
-                                   )
-                                 }
-                               
-                               </div>
+                        projectsData.map((item, index) => (
+                            <div key={`cardItems${item.name.trim()}`} className="project__content__cards__item" onMouseEnter={() => handleHover(index)} onMouseLeave={() => handleHover(null)}>
+                                <div className="project__content__cards__item__img-wrapper">
+                                    <a>
+                                        <img src={item.image} alt={item.name} />
+                                    </a>
+                                </div>
+                                <div className="overlay">
+                                    {index === hoveredValue && (
+                                        <div>
+                                            <p>{item.name}</p>
+                                            <a href={item.link} target="_blank" rel="noopener noreferrer">
+                                                <button>Visit</button>
+                                            </a>
+                                        </div>
+                                    )}
+                                </div>
+
                             </div>
                         ))
                     }
